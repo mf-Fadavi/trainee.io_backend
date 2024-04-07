c75058d1565c02e6a097bcb31c479f92c24cb4f7
Noha.F <fadavinoha@gmail.com>
2024-04-07 21:35:37 +0200

    rename the application

--- organiser/src/main/java/io/trainee/organiser/Application.java
+++ organiser/src/main/java/io/trainee/organiser/Application.java
@@ -2,12 +2,13 @@ package io.trainee.organiser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
-public class DemoApplication {
-
+public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
