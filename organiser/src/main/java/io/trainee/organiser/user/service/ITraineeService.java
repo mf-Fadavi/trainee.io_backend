fdce692e06f4ff9c5abf246a84da1f3b6df50d4d
Noha.F <fadavinoha@gmail.com>
2024-04-07 22:10:48 +0200

    ITraineeService change method type

--- organiser/src/main/java/io/trainee/organiser/user/service/ITraineeService.java
+++ organiser/src/main/java/io/trainee/organiser/user/service/ITraineeService.java
@@ -4,6 +4,7 @@ import io.trainee.organiser.user.entity.TraineeEntity;
 import io.trainee.organiser.user.exception.UserNotFoundException;
 import io.trainee.organiser.user.request.CreateTraineeRequest;
 import io.trainee.organiser.user.request.UpdateTraineeRequest;
+import io.trainee.organiser.user.response.TraineeView;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,7 +14,7 @@ public interface ITraineeService {
 
     List<TraineeEntity> findAll();
     Optional<TraineeEntity> findOneById(UUID traineeId);
-    CreateTraineeRequest createOne(CreateTraineeRequest traineeInfo) throws UserNotFoundException;
+    TraineeView createOne(CreateTraineeRequest traineeInfo) throws UserNotFoundException;
     UpdateTraineeRequest updateOne(UpdateTraineeRequest traineeInfo);
     void deleteOne(UUID traineeId);
 }
