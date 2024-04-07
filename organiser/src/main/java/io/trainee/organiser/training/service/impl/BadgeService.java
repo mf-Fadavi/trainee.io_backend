eaa51aefb3b6c8a30743bbd886363796b2798236
Noha.F <fadavinoha@gmail.com>
2024-04-07 21:44:16 +0200

    implement CRUD methods from IBadgeService

--- organiser/src/main/java/io/trainee/organiser/training/service/impl/BadgeService.java
+++ organiser/src/main/java/io/trainee/organiser/training/service/impl/BadgeService.java
@@ -1,8 +1,42 @@
 package io.trainee.organiser.training.service.impl;
 
+import io.trainee.organiser.training.repository.BadgeRepository;
+import io.trainee.organiser.training.request.CreateBadgeRequest;
+import io.trainee.organiser.training.request.UpdateBadgeRequest;
+import io.trainee.organiser.training.response.BadgeView;
 import io.trainee.organiser.training.service.IBadgeService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.UUID;
+
 @Service
 public class BadgeService implements IBadgeService {
+
+    private BadgeRepository badgeRepository;
+
+    @Override
+    public List<BadgeView> findAll() {
+        return null;
+    }
+
+    @Override
+    public BadgeView findOneById(UUID id) {
+        return null;
+    }
+
+    @Override
+    public BadgeView createOne(CreateBadgeRequest badgeDto) {
+        return null;
+    }
+
+    @Override
+    public BadgeView updateOne(UpdateBadgeRequest badgeDto) {
+        return null;
+    }
+
+    @Override
+    public void deleteOne(UUID id) {
+
+    }
 }
