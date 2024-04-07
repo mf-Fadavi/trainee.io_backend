28efd6cf4b725d26cf97b273f3072c7f71a29c54
Noha.F <fadavinoha@gmail.com>
2024-04-07 22:14:28 +0200

    add unique constraint for role name

--- organiser/src/main/java/io/trainee/organiser/user/entity/RoleEntity.java
+++ organiser/src/main/java/io/trainee/organiser/user/entity/RoleEntity.java
@@ -1,15 +1,18 @@
 package io.trainee.organiser.user.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Data;
 
 import java.util.UUID;
 
+@Data
 @Entity
+@Table(name = "role", uniqueConstraints = @UniqueConstraint(columnNames = {"name"}))
 public class RoleEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+    private UUID roleId;
+
+    @Column(unique = true)
+    private String name;
 }
